5a3ff6e68225b893e5d6b632cb53c1b5eb495303
Michelle <michelle.en4@gmail.com>
2019-10-07 10:06:04 -0400

    Updates

---
+++ precache-manifest.5bdc1dfa43945b5394410b52136d3dea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b58cf5c979ca62bde6f7d78f05d83127",
+    "url": "/take-notes/index.html"
+  },
+  {
+    "revision": "1ead71c85053a3fa398e",
+    "url": "/take-notes/static/css/main.20f92c37.chunk.css"
+  },
+  {
+    "revision": "e6c1c8651bda8a8c6785",
+    "url": "/take-notes/static/js/2.d8c4d936.chunk.js"
+  },
+  {
+    "revision": "1ead71c85053a3fa398e",
+    "url": "/take-notes/static/js/main.4cb2f20e.chunk.js"
+  },
+  {
+    "revision": "14c3bbd44a3645f90ad1",
+    "url": "/take-notes/static/js/runtime~main.064cfc67.js"
+  }
+]);
